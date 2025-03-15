83960b1ffbe59a3863bb851f2164306dd3a99a66
sethww <seth34wafula@gmail.com>
2025-03-16 01:36:00 +0300

    Make underflow card appear once

--- app/src/main/java/com/example/logic3/Interface/DashboardScreen.kt
+++ app/src/main/java/com/example/logic3/Interface/DashboardScreen.kt
@@ -1,5 +1,6 @@
 package com.example.logic3.Interface
-
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,9 +19,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -34,6 +38,8 @@ fun DashboardScreen(
     tracker: BudgetTracker,
     refreshData: () -> Unit
 ) {
+
+    val context = LocalContext.current
     Column(modifier = Modifier.fillMaxSize()) {
         // Budget Summary Card
         Card(
@@ -70,8 +76,12 @@ fun DashboardScreen(
                 }
             }
         }
-        //UI for overflow
-        if (tracker.getRemainingDailyAllocation() > 0) {
+        // Get reference to SharedPreferences
+        val sharedPreferences = LocalContext.current.getSharedPreferences("BudgetAppPrefs", Context.MODE_PRIVATE)
+        // value to record whether the button has been shown
+        val hasShownUnderflowCard = remember { mutableStateOf(sharedPreferences.getBoolean("hasShownUnderflowCard", false)) }
+
+        if (tracker.getRemainingDailyAllocation() > 0 && !hasShownUnderflowCard.value) {
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -97,6 +107,10 @@ fun DashboardScreen(
                             onClick = {
                                 tracker.adjustForUnderflow("reallocate")
                                 refreshData()
+                                // Save to SharedPreferences that card has been shown
+                                sharedPreferences.edit().putBoolean("hasShownUnderflowCard", true).apply()
+                                hasShownUnderflowCard.value = true
+                                Toast.makeText(context, "Reallocated", Toast.LENGTH_LONG).show()
                             },
                             colors = ButtonDefaults.buttonColors(
                                 containerColor = MaterialTheme.colorScheme.primary
@@ -109,6 +123,11 @@ fun DashboardScreen(
                             onClick = {
                                 tracker.adjustForUnderflow("next_day")
                                 refreshData()
+                                // Save to SharedPreferences that card has been shown
+                                sharedPreferences.edit().putBoolean("hasShownUnderflowCard", true).apply()
+                                hasShownUnderflowCard.value = true
+                                Toast.makeText(context, "Added to next day", Toast.LENGTH_LONG).show()
+
                             },
                             colors = ButtonDefaults.buttonColors(
                                 containerColor = MaterialTheme.colorScheme.secondary
@@ -121,6 +140,11 @@ fun DashboardScreen(
                             onClick = {
                                 tracker.adjustForUnderflow("save")
                                 refreshData()
+                                // Save to SharedPreferences that card has been shown
+                                sharedPreferences.edit().putBoolean("hasShownUnderflowCard", true).apply()
+                                hasShownUnderflowCard.value = true
+                                Toast.makeText(context, "Saved", Toast.LENGTH_LONG).show()
+
                             },
                             colors = ButtonDefaults.buttonColors(
                                 containerColor = MaterialTheme.colorScheme.tertiary
